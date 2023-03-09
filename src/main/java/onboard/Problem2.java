d56131918632ce8ffbc1d1504546fa43255b4044
ParkIsComing <maroony55@gmail.com>
2023-03-09 19:10:34 +0900

    ✨ feat: Problem2 풀이 추가

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -1,11 +1,25 @@
 package onboard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
-        Integer[] answer = {};
+        Integer[] answer = new Integer[20];
+
+        for(int i=0; i<20; i++){
+            answer[i] = i+1;
+        }
+        for(Integer[] set: flipCards){
+            Integer [] copiedRange = Arrays.copyOfRange(answer,set[0]-1,set[1]);
+
+            Integer [] flipped = new Integer[copiedRange.length];
+            for(int i=0; i<copiedRange.length;i++){
+                flipped[copiedRange.length - 1 - i] = copiedRange[i];
+            }
+            System.arraycopy(flipped, 0, answer, set[0]-1, copiedRange.length );
+        }
         return answer;
     }
 }
