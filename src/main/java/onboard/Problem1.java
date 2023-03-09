817b6b239f2b43dbdf942b76b3cef7db8cbbe20e
ParkIsComing <maroony55@gmail.com>
2023-03-09 19:05:47 +0900

    ✨ feat: Problem1 풀이 추가

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -5,7 +5,17 @@ import java.util.List;
 
 public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
-        List<Integer> answer = new ArrayList<>();
+        if(minute < 45){
+            if(hour == 0){
+                hour = 23;
+            }else{
+                hour -= 1;
+            }
+            minute = minute + 60 - 45;
+        }else{
+            minute = minute - 45;
+        }
+        List<Integer> answer = List.of(hour, minute);
         return answer;
     }
 }
