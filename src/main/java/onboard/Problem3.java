fc3dd9612351cb868e192616cd8f4acab25fcf1a
ParkIsComing <maroony55@gmail.com>
2023-03-09 19:15:24 +0900

    ✨ feat: Problem3 풀이 추가

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -1,11 +1,18 @@
 package onboard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
-        List<Integer> answer = new ArrayList<>();
+        int sum = 0;
+        for(int num: numbers){
+            sum += num;
+        }
+
+        Collections.sort(numbers);
+        List<Integer> answer =List.of(sum/numbers.size(), numbers.get(2));
         return answer;
     }
 }
